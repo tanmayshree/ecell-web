acd8c8553713af65a15406ad6a10fc76ed14e0d8
Tanmay Shree <tanmay.25ts@gmail.com>
2022-08-14 15:46:08 +0530

    Hero Section New

--- src/pages/Home/Hero_Section.js
+++ src/pages/Home/Hero_Section.js
@@ -1,23 +1,18 @@
 import './Hero.css';
-
+import img from "./../../assets/hero-image00.png"
 const Hero_Section = () => {
     return (
-        <header className='hero-page' data-aos="fade-up"
-            data-aos-offset="200"
-            data-aos-delay="50"
-            data-aos-duration="1000"
-            data-aos-easing="ease-in-out"
-            data-aos-mirror="true"
-            data-aos-once="false"
-            data-aos-anchor-placement="top-center">
-            <div className="hero-content">
-                <div className="hero-title">ENTREPRENEURSHIP CELL</div>
-                <div className="hero-divider"><i class="fa-solid fa-diamond" />
-                    <span className='divider-dash' />
-                    <i class="fa-solid fa-diamond" /></div>
-                <div className="hero-subtitle">National Institute of Technology, Agartala</div>
+        <>
+            <div className="hero-section">
+                <div className="hero-section-content">
+                    <p className="hero-title">Entrepreneurship Cell</p>
+                    <p className="hero-subtitle">National Institute of Technology, Agartala</p>
+                </div>
+                <div className="hero-section-right">
+                    <img src={img} alt="" />
+                </div>
             </div>
-        </header>
+        </>
     );
 }
 
